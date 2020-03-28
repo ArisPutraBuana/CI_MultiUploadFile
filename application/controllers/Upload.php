fd9c1274d1c502920ac409bd4d285cc8e106c264
ArisPutraBuana <arispb99@gmail.com>
2020-03-28 22:12:35 +0700

    -

---
+++ application/controllers/Upload.php
@@ -0,0 +1,26 @@
+<?php
+class Upload extends CI_Controller{
+	function __construct(){
+		parent::__construct();
+
+	}
+	function index(){
+		$this->load->view('v_upload');
+	}
+	function upload_image(){
+		$config['upload_path'] = './assets/images/'; //path folder
+	    $config['allowed_types'] = 'gif|jpg|png|jpeg|bmp'; //type yang dapat diakses bisa anda sesuaikan
+	    $config['encrypt_name'] = TRUE; //nama yang terupload nantinya
+
+	    $this->load->library('upload',$config);
+	    for ($i=1; $i <=5 ; $i++) { 
+	    	if(!empty($_FILES['filefoto'.$i]['name'])){
+	    		if(!$this->upload->do_upload('filefoto'.$i))
+	    			$this->upload->display_errors();	
+	    		else
+	    			echo "Foto berhasil di upload";
+	    	}
+	    }
+				
+	}
+}
